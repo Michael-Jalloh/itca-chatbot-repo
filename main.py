c9ac32d386e1c19569410123bcbdea95151f3d2f
Michael-Jalloh <michaeljalloh19@gmail.com>
2021-06-26 16:28:02 +0000

    Initial Updates

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests as r
+
+token = "1892826956:AAFeIlCxKLADu-9udajcQABhWK151eqhauU"
+base_url = f"https://api.telegram.org/bot{token}/"
+
+data = r.get(base_url+"getupdates").json()
+for message in data["result"]:
+    chat_id = message["message"]["chat"]["id"]
+    #user = message["message"]["chat"]["username"]
+    msg = f"Hello {user}, am Itca_Bot"
+    r.get(base_url+f"sendmessage?text={msg}&chat_id={chat_id}")
